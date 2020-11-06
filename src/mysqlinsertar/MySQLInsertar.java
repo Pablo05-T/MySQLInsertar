f1c846bd2bc3e2fa627ac0a4cd926d77f510a2ec
Pablo05-Tr <72419238+Pablo05-Tr@users.noreply.github.com>
2020-11-05 19:56:19 -0600

    MySQLInsertar

---
+++ src/mysqlinsertar/MySQLInsertar.java
@@ -0,0 +1,34 @@
+package mysqlinsertar;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Pablo
+ */
+public class MySQLInsertar {
+    public static void main(String[] args) {
+        
+        String url = "jdbc:mysql://localhost:3306/libreriadb?useSSL=false";
+        String user = "root";
+        String password = "************";
+        
+        String autor = "Trygve Gulbranssen";
+        String sql = "INSERT INTO Autores(Nombre) VALUES(?)";
+        
+        try (Connection con = DriverManager.getConnection(url, user, password);
+                PreparedStatement pst = con.prepareStatement(sql)){
+            pst.setString(1, autor);
+            pst.executeUpdate();
+            System.out.println("Un nuevo autor ha sido insertado");
+        } catch (SQLException ex){
+            Logger lgr = Logger.getLogger(MySQLInsertar.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
+    
+}
